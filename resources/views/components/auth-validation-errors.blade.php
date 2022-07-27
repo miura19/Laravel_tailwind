0640ebf899aacc0504a5522aaa7108784d86b260
miura19 <littlecloud194@gmail.com>
2022-07-28 00:04:50 +0900

    ★バリデーションメッセージとか★

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,15 +1,6 @@
-@props(['errors'])
+<div class="mt-3 list-disc list-inside text-sm text-red-600">
+    @if ($errors->has($name))
+        <li>{{$errors->first($name)}}</li>
+    @endif
+</div>
 
-@if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
-        </div>
-
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::group(['prefix' => 'post','middleware' => 'auth'], function () {
+    Route::get('create',[PostController::class,'create'])->name('post.create');
+    Route::post('store',[PostController::class,'store'])->name('post.store');
+});
 
 require __DIR__.'/auth.php';
